f8a683e5cfa7bb747d69217cb57a44df6d6fd779
Jonathan Schuh <122762581+jonschuh@users.noreply.github.com~>
2023-03-07 19:26:17 -0600

    first fetch call working

--- assets/script.js
+++ assets/script.js
@@ -1,11 +1,23 @@
 // curl -H "Accept: application/json" https://icanhazdadjoke.com/
+// var jokeApiUrl = 'https://api.humorapi.com/jokes/search?number=1&include-tags=dad&api-key=e4618d46550a4776901fb44e793e790e';
+var breweryApiUrl = 'https://api.openbrewerydb.org/breweries?limit=1by_city=Atlanta';
 
-function getApi() {
-    var requestUrl = 'https://icanhazdadjoke.com/';
-
-    fetch(requestUrl)
+function firstCallAttempt(){
+    fetch(breweryApiUrl)
         .then(function(response) {
-            console.log('response', response)
             return response.json();
         })
-}
+        .then(function(data) {
+            console.log("brewery name", data[0].name);
+            for (var i = 0; i < data.length; i++) {
+            
+                
+
+            }
+        });
+
+    }
+firstCallAttempt();
+
+// searchInput.addEventListener("submit", (event) => {
+//     const { value } = event.target;
