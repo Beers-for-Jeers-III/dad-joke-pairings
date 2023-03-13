8668f74314376f8139900237462ef3a3612670f5
Ryan <pinkstonr54@gmail.com>
2023-03-13 17:39:37 -0500

    added localStorage appending to the app

--- assets/script.js
+++ assets/script.js
@@ -4,6 +4,19 @@ var userInput = "";
 var userValue = document.querySelector("#user-value");
 var searchInput = document.querySelector("#userForm");
 var breweryApiUrl = 'https://api.openbrewerydb.org/breweries?limit=1&by_city=' + userInput
+var infoBox = document.querySelector("#info-box");
+
+//localStorage writing(?)
+var savedBrew = document.createElement("p");
+var savedBrewContent = document.createTextNode(localStorage.getItem("breweryString"));
+savedBrew.appendChild(savedBrewContent);
+infoBox.appendChild(savedBrew);
+
+var savedJoke = document.createElement("p");
+var savedJokeContent = document.createTextNode(localStorage.getItem("jokeString"));
+savedJoke.appendChild(savedJokeContent);
+infoBox.appendChild(savedJoke);
+
 
 //this function will make the URL from user input that will be needed for the brewery API call.
 function urlMaker() {
@@ -19,15 +32,19 @@ function firstCallAttempt (breweryApiUrl) {
   fetch(breweryApiUrl)
     .then(function(response) {
       return response.json();
-      })
+    })
     .then(function (data) {
       //console.log("brewery name", data[0].name);
       var brewName = document.createElement("p");
       var brewContent = document.createTextNode(data[0].name);
+      brewName.setAttribute("id", "brewElement");
       brewName.appendChild(brewContent);
-      var infoBox = document.querySelector("#info-box");
-                infoBox.appendChild(brewName);
-        }
+      infoBox.appendChild(brewName);
+      localStorage.setItem("breweryString", data[0].name);
+      var savedDataBrew = localStorage.getItem("breweryString");
+      document.querySelector("#brewElement").innerHTML = savedDataBrew;
+
+    }
 )};
 
 //this function calls the joke api and appends the joke to the page
@@ -44,10 +61,13 @@ function getBadJoke() {
   console.log(data)
   var joke = data.joke;
   var jokeElement = document.createElement("p");
+  jokeElement.setAttribute("id", "jokeElement");
   var jokeContent = document.createTextNode(joke);
   jokeElement.appendChild(jokeContent);
-  var infoBox = document.querySelector("#info-box");
   infoBox.appendChild(jokeElement);
+  localStorage.setItem("jokeString", joke);
+  var savedDataJoke = localStorage.getItem("jokeString");
+  document.querySelector("#jokeElement").innerHTML = savedDataJoke;
 })
 }
 
